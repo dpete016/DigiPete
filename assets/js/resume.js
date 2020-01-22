c036ae3c9283dd0d90f963d4bc006140b569eae9
Dpete016 <dpete016@gmail.com>
2020-01-22 00:21:50 -0500

    wip

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -54,10 +54,10 @@ $('.portfolio-categ li').click(function (e) {
 });
 
 jQuery("a[rel^='prettyPhoto']").prettyPhoto({
-  animationSpeed:'fast',
-  slideshow:5000,
-  theme:'light_rounded',
-  show_title:false,
+  animationSpeed: 'fast',
+  slideshow: 5000,
+  theme: 'light_rounded',
+  show_title: false,
   overlay_gallery: false
 });
 
